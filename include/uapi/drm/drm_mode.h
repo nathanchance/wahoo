dc907a70d5de72260e01903c8d0b9f33a3f76717
Shashank Sharma <shashank.sharma@intel.com>
2016-10-17 17:34:37 +0530

    drm: add picture aspect ratio flags
    This patch adds drm flag bits for aspect ratio information
    
    Currently drm flag bits don't have field for mode's picture
    aspect ratio. This field will help the driver to pick mode with
    right aspect ratio, and help in setting right VIC field in avi
    infoframes.
    
    V2: Addressed review comments from Sean
    - Changed PAR-> PIC_AR
    V3: Rebase
    V3: Added r-b by Jose
    
    Change-Id: I40cd7fc8393166b710a5f1faaea6e7dac59af65b
    Signed-off-by: Shashank Sharma <shashank.sharma@intel.com>
    Reviewed-by: Jim Bride <jim.bride@linux.intel.com>
    Reviewed-by: Jose Abreu <Jose.Abreu@synopsys.com>
    Cc: Daniel Vetter <daniel.vetter@ffwll.ch>
    Cc: Emil Velikov <emil.l.velikov@gmail.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: http://patchwork.freedesktop.org/patch/msgid/1476705880-15600-2-git-send-email-shashank.sharma@intel.com
    Git-commit: 876f43c073d79ad3f14a4cebd1aea1f39fc4daf5
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    [nbahrani@codeaurora.org: resolve merge conflicts by changing
     the mask bits to 27:24 instead of 22:19]
    Signed-off-by: Navid Bahrani <nbahrani@codeaurora.org>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- include/uapi/drm/drm_mode.h
+++ include/uapi/drm/drm_mode.h
@@ -75,6 +75,20 @@
 #define  DRM_MODE_FLAG_SEAMLESS			(1<<19)
 
 
+/* Picture aspect ratio options */
+#define DRM_MODE_PICTURE_ASPECT_NONE		0
+#define DRM_MODE_PICTURE_ASPECT_4_3		1
+#define DRM_MODE_PICTURE_ASPECT_16_9		2
+
+/* Aspect ratio flag bitmask (4 bits 27:24) */
+#define DRM_MODE_FLAG_PIC_AR_MASK		(0x0F<<24)
+#define  DRM_MODE_FLAG_PIC_AR_NONE \
+			(DRM_MODE_PICTURE_ASPECT_NONE<<24)
+#define  DRM_MODE_FLAG_PIC_AR_4_3 \
+			(DRM_MODE_PICTURE_ASPECT_4_3<<24)
+#define  DRM_MODE_FLAG_PIC_AR_16_9 \
+			(DRM_MODE_PICTURE_ASPECT_16_9<<24)
+
 /* DPMS flags */
 /* bit compatible with the xorg definitions. */
 #define DRM_MODE_DPMS_ON	0
@@ -88,11 +102,6 @@
 #define DRM_MODE_SCALE_FULLSCREEN	1 /* Full screen, ignore aspect */
 #define DRM_MODE_SCALE_CENTER		2 /* Centered, no scaling */
 #define DRM_MODE_SCALE_ASPECT		3 /* Full screen, preserve aspect */
-
-/* Picture aspect ratio options */
-#define DRM_MODE_PICTURE_ASPECT_NONE	0
-#define DRM_MODE_PICTURE_ASPECT_4_3	1
-#define DRM_MODE_PICTURE_ASPECT_16_9	2
 
 /* Dithering mode options */
 #define DRM_MODE_DITHERING_OFF	0
