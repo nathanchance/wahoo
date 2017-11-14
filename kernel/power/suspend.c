85d066c05dbdd7483aa7c5d1a6ecd52a1ac11898
Joe Maples <joe@frap129.org>
2017-11-14 17:48:03 -0500

    PM: Port fingprintd quick thaw from OnePlus
    Pixel 2 XL's process is named fingerprint, not fingerprintd,
    so adjust accordingly. Don't say I'm wrong, try to pgrep
    fingerprintd first.
    
    Signed-off-by: Joe Maples <joe@frap129.org>

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -313,6 +313,9 @@ void __weak arch_suspend_enable_irqs(void)
  *
  * This function should be called after devices have been suspended.
  */
+#ifdef CONFIG_QUICK_THAW_FINGERPRINTD
+extern void thaw_fingerprintd(void);
+#endif
 static int suspend_enter(suspend_state_t state, bool *wakeup)
 {
 	int error, last_dev;
@@ -398,6 +401,9 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 
  Platform_wake:
 	platform_resume_noirq(state);
+#ifdef CONFIG_QUICK_THAW_FINGERPRINTD
+	thaw_fingerprintd();
+#endif
 	dpm_resume_noirq(PMSG_RESUME);
 
  Platform_early_resume:
