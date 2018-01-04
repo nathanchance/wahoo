daf1d63a594b001097224036bf31d3aa180811a3
Harsh Shandilya <harsh@prjkt.io>
2018-01-04 18:19:18 +0530

    qcom: msm-core: Revert a couple of patches.
    This reverts commits 9fccd0a6dca5bb0df5bbfc709227638c8212149f and
    70c967a49be00d2ca4eba0e72112fe0429e7603a to clean the way for the
    following patches.
    
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- drivers/power/qcom/msm-core.c
+++ drivers/power/qcom/msm-core.c
@@ -308,7 +308,7 @@ static __ref int do_sampling(void *data)
 	static int prev_temp[NR_CPUS];
 
 	while (!kthread_should_stop()) {
-		wait_for_completion_interruptible(&sampling_completion);
+		wait_for_completion(&sampling_completion);
 		cancel_delayed_work(&sampling_work);
 
 		mutex_lock(&kthread_update_mutex);
@@ -332,8 +332,7 @@ static __ref int do_sampling(void *data)
 		if (!poll_ms)
 			goto unlock;
 
-		queue_delayed_work(system_power_efficient_wq,
-			&sampling_work,
+		schedule_delayed_work(&sampling_work,
 			msecs_to_jiffies(poll_ms));
 unlock:
 		mutex_unlock(&kthread_update_mutex);
