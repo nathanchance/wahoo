nathanchance/wahoo

commit f86ee348ef8d342d1da4b861258a978c42640c2f

wahoo: Fix all outstanding GCC warnings
https://toolchains.bootlin.com
https://releases.linaro.org/components/toolchain/binaries/latest-7/aarch64-linux-gnu/

Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

drivers/input/touchscreen/lge/lgsic/touch_sw49408.c
@@ -2631,6 +2631,7 @@ int sw49408_check_status(struct device *dev)
 	return ret;
 }
 
+#if 0
 static ssize_t sw49408_get_rn_data(struct device *dev, int16_t *buf, int frame_size)
 {
 	u32 data_offset = 0;
@@ -2646,6 +2647,7 @@ static ssize_t sw49408_get_rn_data(struct device *dev, int16_t *buf, int frame_s
 
 	return 0;
 }
+#endif
 
 int sw49408_irq_abs_data(struct device *dev)
 {
@@ -2787,6 +2789,7 @@ int sw49408_irq_lpwg(struct device *dev)
 	return ret;
 }
 
+#if 0
 void sw49408_irq_runtime_engine_debug(struct device *dev)
 {
 	struct sw49408_data *d = to_sw49408_data(dev);
@@ -2874,6 +2877,7 @@ error:
 	if (rndata != NULL)
 		kfree(rndata);
 }
+#endif
 
 int sw49408_irq_handler(struct device *dev)
 {
@@ -2895,9 +2899,11 @@ int sw49408_irq_handler(struct device *dev)
 		ret = sw49408_irq_lpwg(dev);
 	}
 
+#if 0
 	if (atomic_read(&ts->state.debug_option_mask) & DEBUG_OPTION_2)
 		if (d->info.debug.runtime_dbg_inttype > 0)
 			sw49408_irq_runtime_engine_debug(dev);
+#endif
 
 error:
 	return ret;

Makefile
@@ -640,8 +640,6 @@ KBUILD_CFLAGS	+= $(call cc-option,-fno-delete-null-pointer-checks,)
 KBUILD_CFLAGS	+= $(call cc-disable-warning,maybe-uninitialized,)
 KBUILD_CFLAGS	+= $(call cc-disable-warning,frame-address,)
 KBUILD_CFLAGS	+= $(call cc-disable-warning, format-truncation)
-KBUILD_CFLAGS	+= $(call cc-disable-warning, format-overflow)
-KBUILD_CFLAGS	+= $(call cc-disable-warning, int-in-bool-context)
 
 ifdef CONFIG_LD_DEAD_CODE_DATA_ELIMINATION
 KBUILD_CFLAGS	+= $(call cc-option,-ffunction-sections,)

drivers/crypto/msm/qce50.c
@@ -4675,7 +4675,7 @@ again:
 			pce_dev->intr_cadence = 0;
 			atomic_set(&pce_dev->bunch_cmd_seq, 0);
 			atomic_set(&pce_dev->last_intr_seq, 0);
-			pce_dev->cadence_flag = ~pce_dev->cadence_flag;
+			pce_dev->cadence_flag = !pce_dev->cadence_flag;
 		}
 	}
 

drivers/gpio/gpiolib-acpi.c
@@ -189,7 +189,7 @@ static acpi_status acpi_gpiochip_request_interrupt(struct acpi_resource *ares,
 
 	if (pin <= 255) {
 		char ev_name[5];
-		sprintf(ev_name, "_%c%02X",
+		sprintf(ev_name, "_%c%02hhX",
 			agpio->triggering == ACPI_EDGE_SENSITIVE ? 'E' : 'L',
 			pin);
 		if (ACPI_SUCCESS(acpi_get_handle(handle, ev_name, &evt_handle)))

drivers/input/misc/vl53L0/src/vl53l010_api.c
@@ -615,10 +615,10 @@ VL53L0_Error VL53L010_StaticInit(VL53L0_DEV Dev)
     if (Status == VL53L0_ERROR_NONE)
         Status = VL53L0_WrByte(Dev, 0x88, 0x00);
 
-	/* this function do nothing if it has been called before */
-	Status = VL53L010_get_info_from_device(Dev);
+    /* this function do nothing if it has been called before */
+    Status = VL53L010_get_info_from_device(Dev);
 
-	if (Status == VL53L0_ERROR_NONE) {
+    if (Status == VL53L0_ERROR_NONE) {
         Revision = VL53L010_GETDEVICESPECIFICPARAMETER(Dev, Revision);
     }
 

drivers/input/misc/vl53L0/src/vl53l0_api.c
@@ -453,13 +453,13 @@ VL53L0_Error VL53L0_DataInit(VL53L0_DEV Dev)
         Status = VL53L0_SetLimitCheckEnable(Dev,
                 VL53L0_CHECKENABLE_RANGE_IGNORE_THRESHOLD, 0);
 
-	if (Status == VL53L0_ERROR_NONE)
-		Status = VL53L0_SetLimitCheckEnable(Dev,
-			VL53L0_CHECKENABLE_SIGNAL_RATE_MSRC, 0);
+    if (Status == VL53L0_ERROR_NONE)
+        Status = VL53L0_SetLimitCheckEnable(Dev,
+                VL53L0_CHECKENABLE_SIGNAL_RATE_MSRC, 0);
 
-	if (Status == VL53L0_ERROR_NONE)
-		Status = VL53L0_SetLimitCheckEnable(Dev,
-			VL53L0_CHECKENABLE_SIGNAL_RATE_PRE_RANGE, 0);
+    if (Status == VL53L0_ERROR_NONE)
+        Status = VL53L0_SetLimitCheckEnable(Dev,
+                VL53L0_CHECKENABLE_SIGNAL_RATE_PRE_RANGE, 0);
 
     /* Limit default values */
     if (Status == VL53L0_ERROR_NONE) {

drivers/input/touchscreen/lge/lgsic/touch_sw49408.h
@@ -597,7 +597,9 @@ int sw49408_tc_driving(struct device *dev, int mode);
 int sw49408_irq_abs(struct device *dev);
 int sw49408_irq_abs_data(struct device *dev);
 int sw49408_irq_lpwg(struct device *dev);
+#if 0
 void sw49408_irq_runtime_engine_debug(struct device *dev);
+#endif
 int sw49408_irq_handler(struct device *dev);
 int sw49408_check_status(struct device *dev);
 int sw49408_chip_info_load(struct device* dev);

drivers/input/touchscreen/lge/lgsic/touch_sw49408_abt.c
@@ -1076,8 +1076,10 @@ int sw49408_sic_abt_irq_handler(struct device *dev)
 		}
 	}
 
+#if 0
 	if ((abt_conn_tool == NOTHING) && (d->info.debug.runtime_dbg_inttype > 0))
 		sw49408_irq_runtime_engine_debug(dev);
+#endif
 
 error:
 	return ret;

commit f26cf85eaf259e594c15a29afe93d34bc3c6e861

input: touchscreen: Fix unused variable warning
Fixes: f86ee348ef8d ("wahoo: Fix all outstanding GCC warnings")
Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

drivers/input/touchscreen/lge/lgsic/touch_sw49408.c
@@ -2881,7 +2881,9 @@ error:
 
 int sw49408_irq_handler(struct device *dev)
 {
+#if 0
 	struct touch_core_data *ts = to_touch_core(dev);
+#endif
 	struct sw49408_data *d = to_sw49408_data(dev);
 	int ret = 0;
 