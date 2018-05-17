4a965bc5eab65b4d5fcf7a7c91478c59f4b81e20
Nathan Chancellor <natechancellor@gmail.com>
2018-05-16 17:21:17 -0700

    wake_gestures: Remove cmdline boilerplate
    Use the recently created function to check if the kernel should change
    the sweep values if the device is a Pixel 2.
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/input/touchscreen/wake_gestures.c
+++ drivers/input/touchscreen/wake_gestures.c
@@ -33,6 +33,7 @@
 #include <linux/hrtimer.h>
 #include <asm-generic/cputime.h>
 #include <linux/wakelock.h>
+#include <linux/wahoo_info.h>
 
 /* Tunables */
 #define WG_DEBUG		0
@@ -127,27 +128,7 @@ static struct work_struct s2w_input_work;
 static struct work_struct dt2w_input_work;
 static struct wake_lock dt2w_wakelock;
 
-//get hardware type
 static int hw_version = TAIMEN;
-static int __init get_model(char *cmdline_model)
-{
-	if (strstr(cmdline_model, "walleye")) {
-		sweep_y_limit = SWEEP_Y_LIMIT_WALLEYE;
-		sweep_x_limit = SWEEP_X_LIMIT_WALLEYE;
-		sweep_x_b1 = SWEEP_X_B1_WALLEYE;
-		sweep_x_b2 = SWEEP_X_B2_WALLEYE;
-		sweep_y_start = SWEEP_Y_START_WALLEYE;
-		sweep_x_start = SWEEP_X_START_WALLEYE;
-		sweep_x_final = SWEEP_X_FINAL_WALLEYE;
-		sweep_y_next = SWEEP_Y_NEXT_WALLEYE;
-		sweep_x_max = SWEEP_X_MAX_WALLEYE;
-		sweep_edge = SWEEP_EDGE_WALLEYE;
-		hw_version = WALLEYE;
-	}
-
-	return 0;
-}
-__setup("androidboot.hardware=", get_model);
 
 static bool is_suspended(void)
 {
@@ -732,6 +713,20 @@ extern struct kobject *android_touch_kobj;
 static int __init wake_gestures_init(void)
 {
 	int rc = 0;
+
+	if (is_google_walleye()) {
+		sweep_y_limit = SWEEP_Y_LIMIT_WALLEYE;
+		sweep_x_limit = SWEEP_X_LIMIT_WALLEYE;
+		sweep_x_b1 = SWEEP_X_B1_WALLEYE;
+		sweep_x_b2 = SWEEP_X_B2_WALLEYE;
+		sweep_y_start = SWEEP_Y_START_WALLEYE;
+		sweep_x_start = SWEEP_X_START_WALLEYE;
+		sweep_x_final = SWEEP_X_FINAL_WALLEYE;
+		sweep_y_next = SWEEP_Y_NEXT_WALLEYE;
+		sweep_x_max = SWEEP_X_MAX_WALLEYE;
+		sweep_edge = SWEEP_EDGE_WALLEYE;
+		hw_version = WALLEYE;
+	}
 
 	wake_dev = input_allocate_device();
 	if (!wake_dev) {
